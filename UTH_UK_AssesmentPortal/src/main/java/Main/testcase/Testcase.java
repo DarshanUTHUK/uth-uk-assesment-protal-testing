bc321cfeefe6ff7cf433f2a4a058ba96859d07d5
Shreyank M <110959617+shallu99@users.noreply.github.com>
2023-02-23 13:48:40 +0530

    added login testcase

---
+++ UTH_UK_AssesmentPortal/src/main/java/Main/testcase/Testcase.java
@@ -0,0 +1,37 @@
+package Main.testcase;
+import Com.utils.excelExportAndFileIO.Exceltable;
+import Reuseable.testcase.Uth_Uk_Login;
+import Reuseable.testcase.Uth_uk_Register;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.*;
+
+import java.io.IOException;
+
+public class Testcase extends BaseTestNG {
+     WebDriver driver;
+    @DataProvider(name="TestScript_1")
+    public Object[][] dataRegister(){
+        Exceltable Data = new Exceltable();
+        Data.readExceltable("src/main/resources/", "TestData.xlsx", "Module_1", "TestScript_1");
+        return Data.ExcelData();
+    }
+@Test(dataProvider = "TestScript_1" ,suiteName = "Register")
+public void Register_Test(String Email,String Username,String Password,String Execute) throws IOException {
+ Uth_uk_Register myRegister = new Uth_uk_Register();
+//    Reporter.log(driver.getTitle(),true);
+    myRegister.uthuk_Assesment_Register(Email,Username,Password);
+
+    }
+    @DataProvider(name="TestScript_2")
+    public Object[][] dataLogin(){
+        Exceltable Data = new Exceltable();
+        Data.readExceltable("src/main/resources/", "TestData.xlsx", "Module_1", "TestScript_2");
+        return Data.ExcelData();
+    }
+    @Test(dataProvider = "TestScript_2" ,suiteName = "Login")
+    public void Login_Test(String Username,String Password,String Execute) throws IOException {
+        Uth_Uk_Login myLogin = new Uth_Uk_Login();
+       myLogin.uthuk_Assesment_Username(Username);
+       myLogin.uthuk_Assesment_Password(Password);
+    }
+}
