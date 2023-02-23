682998611cb4a4c753569ae82888e8eea7035dc8
DarshanUTHUK <darshan@uth-uk.com>
2023-02-23 12:09:55 +0530

    add something
    add something

--- UTH_UK_AssesmentPortal/src/main/java/Reuseable/testcase/Uth_uk_assesment.java
+++ UTH_UK_AssesmentPortal/src/main/java/Reuseable/testcase/Uth_uk_assesment.java
@@ -1,19 +1,20 @@
 package Reuseable.testcase;
 
 import Com.Base.resources.base;
+import Main.testcase.BaseTestNG;
 import org.openqa.selenium.WebDriver;
 import uth_uk_assesment.page.RegisterPage;
 
 
 import java.io.IOException;
 
-public class Uth_uk_assesment extends base {
+public class Uth_uk_assesment extends BaseTestNG {
 WebDriver driver;
 
  RegisterPage Register;
 
     public WebDriver uthuk_Assesment_Register(String Email,String Username,String Password) throws IOException {
-        
+
         Register = new RegisterPage(driver);
         Register.Regsiter_Now.click();
         Register.Email.sendKeys(Email);
